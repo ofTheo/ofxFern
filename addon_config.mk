da992379602fb73565da399ec45e7f85dc2e77b0
Robert Xiao <brx@cs.cmu.edu>
2015-02-24 16:03:15 -0500

    Add zlib headers to fix a compile error with MSVC.

--- addon_config.mk
+++ addon_config.mk
@@ -6,6 +6,8 @@ meta:
 
 common:
 	ADDON_DEPENDENCIES = ofxOpenCv
+	# Include zlib headers for Windows (zlib libraries are included in ofxOpenCv)
+	ADDON_INCLUDES += include
 	ADDON_SOURCES_EXCLUDE = AUTHORS COPYING README
 	ADDON_SOURCES_EXCLUDE += Makefile Makefile.icc
 	ADDON_SOURCES_EXCLUDE += model.bmp model.bmp.roi
