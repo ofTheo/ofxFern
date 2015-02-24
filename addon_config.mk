1a79f1b71f931d44c20f878d85fe630c7e4f1828
Robert Xiao <brx@cs.cmu.edu>
2015-02-24 15:57:51 -0500

    Exclude non-source files from the addon.
    This fixes compile errors with MSVC due to non-source files.

---
+++ addon_config.mk
@@ -0,0 +1,11 @@
+meta:
+	ADDON_NAME = ofxFern
+	ADDON_DESCRIPTION = An implementation of the Fern tracker from EPFL CVLab for OpenFrameworks.
+	ADDON_AUTHOR = Theodore Watson
+	ADDON_URL = https://github.com/ofTheo/ofxFern
+
+common:
+	ADDON_DEPENDENCIES = ofxOpenCv
+	ADDON_SOURCES_EXCLUDE = AUTHORS COPYING README
+	ADDON_SOURCES_EXCLUDE += Makefile Makefile.icc
+	ADDON_SOURCES_EXCLUDE += model.bmp model.bmp.roi
